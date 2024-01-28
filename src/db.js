08c39f2b8a0838b8d01c4d5bfe28e92013467379
Eugenio Sgobba <e.sgobba@reply.it>
2024-01-28 22:39:12 +0100

    sostituzione db locale con db AWS (11)

--- src/db.js
+++ src/db.js
@@ -1,9 +1,10 @@
-const mysql = require('mysql')
+const mysql = require('mysql2');
+
 const db = mysql.createConnection({
-host: "dbcomuni.cbk8uqwo8kd7.eu-west-3.rds.amazonaws.com",
-user: "admin",
-password: "progettoReact",
-database:"comuni"
+  host: 'dbcomuni.cbk8uqwo8kd7.eu-west-3.rds.amazonaws.com',
+  user: 'admin',
+  password: 'progettoReact',
+  database: 'comuni',
 });
 
 module.exports = db;
