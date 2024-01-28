606db32528b33a8b9897d431a2bbd2fe56b6dde2
Eugenio Sgobba <e.sgobba@reply.it>
2024-01-28 15:31:33 +0100

    sostituzione db locale con db AWS (6)

--- src/FormMeteo.js
+++ src/FormMeteo.js
@@ -16,7 +16,7 @@ const FormMeteo = () => {
     // Stato per gestire la lista dei comuni
     const [comuni, setComuni] = useState([]);
     useEffect(() => {
-      axios.get('https://primo-sito.vercel.app/api/comuni')
+      axios.get('https://localhost/api/comuni')
         .then(response => {
           setComuni(response.data);
           console.log(response);
