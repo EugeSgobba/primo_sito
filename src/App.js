10bc68eb537368f97a559fe3dbd98cc2f6a5c9ae
Eugenio Sgobba <e.sgobba@reply.it>
2024-01-31 15:04:26 +0100

    sostituzione db locale con db AWS (14)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,74 @@
-import React from 'react';
-import FormMeteo from './FormMeteo';
-import Titolo from './Titolo';
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  Button,
+  Heading,
+  View,
+  withAuthenticator,
+  Image,
+  Card,
+} from "@aws-amplify/ui-react";
 
-const App = () => {
+
+const App = ({ signOut }) => {
+  /*const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
+    setNotes(notesFromAPI);
+  }
+
+  async function createNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const image = form.get("image");
+    const data = {
+      name: form.get("name"),
+      description: form.get("description"),
+      image: image.name,
+    };
+    if (!!data.image) await Storage.put(data.name, image);
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: data },
+    });
+    fetchNotes();
+    event.target.reset();
+  }
+
+  async function deleteNote({ id, name }) {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+    await Storage.remove(name);
+    await API.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
+  }*/
+
   return (
-    <div>
-      <Titolo/>
-      <FormMeteo />
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo"/>
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 };
 
-export default App;
+export default withAuthenticator(App);
--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'dbcomuni.cbk8uqwo8kd7.eu-west-3.rds.amazonaws.com',
+  port: '3306',
   user: 'admin',
   password: 'progettoReact',
   database: 'comuni',
